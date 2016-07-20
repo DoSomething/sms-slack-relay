2a9119c5460a0b122ce72b6fb8efb967e2cac279
Matt Holford <mholford@dosomething.org>
2016-07-20 13:17:55 -0400

    Removes test message; reads & logs form data from Twilio

--- sms_slack/main.py
+++ sms_slack/main.py
@@ -14,20 +14,30 @@ except ImportError:
 
 app = Flask(__name__)
 
+# Twilio credentials
 app.config['TWILIO_ACCOUNT_SID'] = os.environ['TWILIO_ACCOUNT_SID']
 app.config['TWILIO_AUTH_TOKEN'] = os.environ['TWILIO_AUTH_TOKEN']
 
 # To verify incoming requests
 app.config['INCOMING_REQUEST_TOKEN'] = os.environ['INCOMING_REQUEST_TOKEN']
 
+# Slack webhook
+app.config['SLACK_WEBHOOK_URL'] = os.environ['SLACK_WEBHOOK_URL']
+
+# Debug mode
+app.config['DEBUG'] = os.environ['DEBUG']
+
 @app.route('/')
 def hello():
     client = TwilioRestClient(app.config['TWILIO_ACCOUNT_SID'], app.config['TWILIO_AUTH_TOKEN'])
-    message = client.messages.create(to="+15084637343", from_="+12027538383",
-                                     body="Hello there!")
+    # message = client.messages.create(to="+15084637343", from_="+12027538383",
+                                     # body="Hello there!")
     return 'You are here'
 
-@app.route('/sms_to_slack', methods=['POST'])
+@app.route('/sms_to_slack/<request_token>', methods=['POST'])
 def route_to_slack():
     # Stuff here
+    app.logger.debug("Received request with token = %s", request_token)
+    app.logger.debug(request.form)
+
     return ''
