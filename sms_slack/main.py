aae55a6208c766602845e61c99fc9cba9bffa528
Matt Holford <mholford@dosomething.org>
2016-07-20 12:02:49 -0400

    Adds env var reading, basic Flask setup

--- sms_slack/main.py
+++ sms_slack/main.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
+import json, os
+
+try:
+    from flask.ext.cors import CORS  # The typical way to import flask-cors
+except ImportError:
+    # Path hack allows examples to be run without installation.
+    parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    os.sys.path.insert(0, parentdir)
+
+    from flask.ext.cors import CORS
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app.config['TWILIO_ACCOUNT_SID'] = os.environ['TWILIO_ACCOUNT_SID']
+app.config['TWILIO_AUTH_TOKEN'] = os.environ['TWILIO_AUTH_TOKEN']
 
+# To verify incoming requests
+app.config['INCOMING_REQUEST_TOKEN'] = os.environ['INCOMING_REQUEST_TOKEN']
+
+@app.route('/')
+def hello():
+    return 'You are here'
